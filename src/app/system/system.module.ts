f9bc4299976091a5bde5c29f9272227346c6c853
aratakz <danielpessoa432@gmail.com>
2025-06-04 16:36:03 -0300

    headbar

--- src/app/system/system.module.ts
+++ src/app/system/system.module.ts
@@ -5,13 +5,15 @@ import { SystemRoutingModule } from './system-routing.module';
 import { SystemComponent } from './system.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 
 @NgModule({
   declarations: [
     SystemComponent,
     DashboardComponent,
-    NavbarComponent
+    NavbarComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
