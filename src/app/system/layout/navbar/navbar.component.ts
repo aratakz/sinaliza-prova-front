8347ef637c9e946a14367f36f1f778f9e1811f0e
arata <danielpessoa432@gmail.com>
2025-06-12 21:39:05 -0300

    melhoramento do controle de collapse

--- src/app/system/layout/navbar/navbar.component.ts
+++ src/app/system/layout/navbar/navbar.component.ts
@@ -12,6 +12,10 @@ export class NavbarComponent {
     isCollapsed = false;
 
     constructor(private globalService: GlobalService ) {
+      if (window.localStorage.getItem("collapse")) {
+        this.collapseControl = 'collapse';
+        this.isCollapsed = true;
+      }
       globalService.navbarBehavior.next(this.collapseControl)
     }
 
@@ -19,10 +23,12 @@ export class NavbarComponent {
       if (!this.isCollapsed) {
           this.isCollapsed = true;
           this.collapseControl = 'collapse';
+          window.localStorage.setItem('collapse', JSON.stringify(this.isCollapsed));
           this.globalService.navbarBehavior.next(this.collapseControl);
       } else {
         this.isCollapsed = false;
         this.collapseControl = '';
+        window.localStorage.removeItem('collapse');
         this.globalService.navbarBehavior.next(this.collapseControl);
       }
     }
