f41292d11de2b2bb89a522e35ab6b970a13cea25
arata <danielpessoa432@gmail.com>
2025-08-31 10:32:37 -0300

    refactor

--- src/app/system/discipline/components/list/list.component.ts
+++ src/app/system/discipline/components/list/list.component.ts
@@ -36,40 +36,7 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
     this.globalService.activeRouteBehavior.next('Disciplinas');
     this.disciplineService.getAll().subscribe({
-      next: (disciplines: any) => {
-        for (const discipline of disciplines) {
-          const fields: TableData[] = [];
-          const field: TableData = {
-            index: new Date().toISOString(),
-            value: discipline.name
-          };
-          const lineActions: TableData = {
-            index: new Date().toISOString(),
-            classes: ['align-right'],
-            buttons: [
-              {
-                index: new Date().toISOString(),
-                classes: ['btn-edit'],
-                icon: 'fi fi-rr-pencil',
-                onClick: async () => this.onEdit(discipline.id),
-              },
-              {
-                index: new Date().toISOString(),
-                classes: ['btn-remove'],
-                icon: 'fi fi-rr-trash',
-                onClick: async () => await this.onRemove(discipline.id)
-              }
-            ]
-          };
-          fields.push(field);
-          fields.push(lineActions);
-          this.tableData.push({
-            index: Date.toString(),
-            fields: fields
-          });
-        }
-
-      }
+      next: (disciplines: any) => this.feedTable(disciplines),
     });
   }
 
@@ -90,6 +57,41 @@ export class ListComponent implements OnInit {
 
   async onEdit(disciplineId: string) {
     await this.router.navigate([`system/discipline/form/${disciplineId}`])
+  }
+
+
+  feedTable(disciplines:any) {
+    for (const discipline of disciplines) {
+      const fields: TableData[] = [];
+      const field: TableData = {
+        index: new Date().toISOString(),
+        value: discipline.name
+      };
+      const lineActions: TableData = {
+        index: new Date().toISOString(),
+        classes: ['align-right'],
+        buttons: [
+          {
+            index: new Date().toISOString(),
+            classes: ['btn-edit'],
+            icon: 'fi fi-rr-pencil',
+            onClick: async () => this.onEdit(discipline.id),
+          },
+          {
+            index: new Date().toISOString(),
+            classes: ['btn-remove'],
+            icon: 'fi fi-rr-trash',
+            onClick: async () => await this.onRemove(discipline.id)
+          }
+        ]
+      };
+      fields.push(field);
+      fields.push(lineActions);
+      this.tableData.push({
+        index: Date.toString(),
+        fields: fields
+      });
+    }
   }
 
 }
