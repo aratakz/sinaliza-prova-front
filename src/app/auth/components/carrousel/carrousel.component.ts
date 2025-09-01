b45d2f115207a3869eb5cb6df81f2dc8156c1620
arata <danielpessoa432@gmail.com>
2025-09-01 00:52:07 -0300

    adjust banner

--- src/app/auth/components/carrousel/carrousel.component.ts
+++ src/app/auth/components/carrousel/carrousel.component.ts
@@ -12,51 +12,25 @@ export class CarrouselComponent implements OnInit {
 
   images= [
     './assets/carrousel_1.png',
+    './assets/carrousel_2.png',
+    './assets/carrousel_3.png',
   ];
-
-  texts = [
-    'txto 1',
-    'txto 2',
-    'txto 3',
-  ];
-
   ngOnInit(): void {
     this.goNext();
     setInterval(() => {
       this.goNext();
-    }, 5000);
+    }, 10000);
   }
 
   goNext(): void {
     if (!this.currentImage) {
       this.currentImage = this.images[0];
-      this.currentText = this.texts[0];
     } else {
       const nextImageIndex = (this.images.indexOf(this.currentImage) + 1);
       if (nextImageIndex >= this.images.length) {
           this.currentImage = this.images[0];
-          this.currentText = this.texts[0];
       } else {
         this.currentImage = this.images[nextImageIndex];
-        this.currentText = this.texts[nextImageIndex];
-      }
-    }
-  }
-
-  goBack(): void {
-    if (this.currentImage) {
-      const nextImageIndex = (this.images.indexOf(this.currentImage) - 1);
-      if (nextImageIndex < 0) {
-        this.currentImage = this.images[this.images.length - 1];
-        this.currentText = this.texts[this.texts.length - 1];
-      } else {
-        if (nextImageIndex == 0) {
-          this.currentImage = this.images[0];
-          this.currentText = this.texts[0];
-        } else {
-          this.currentImage = this.images[nextImageIndex];
-          this.currentText = this.texts[nextImageIndex];
-        }
       }
     }
   }
